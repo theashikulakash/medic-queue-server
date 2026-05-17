47fbf551419b5e1e087dac30b224c1a660848637
Md Ashikul Islam <akashashikul@gmail.com>
2026-05-17 22:21:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = 2026;
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
