0ff4527b4bbeddd469f81b43f735e0a4e2c60e78
Md Ashikul Islam <akashashikul@gmail.com>
2026-05-17 22:36:55 +0600

    dotenv installed and added

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+dotenv.config();
 
-const PORT = 2026;
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+const PORT = process.env.PORT;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
   res.send("Server is running fine!");
