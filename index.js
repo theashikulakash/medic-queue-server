193eb0db185a47df07db50a4fbeb5d958d2b0031
Md Ashikul Islam <akashashikul@gmail.com>
2026-05-20 20:47:32 +0600

    user info update api added

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const cors = require('cors');
 const dotenv = require("dotenv");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 dotenv.config();
 
 const uri = process.env.MONGODB_URI;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 const PORT = process.env.PORT;
 
 const client = new MongoClient(uri, {
@@ -16,6 +20,30 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
+const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    console.log(payload);
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
+
+
 async function run() {
   try {
     // create db or call db
@@ -23,6 +51,8 @@ async function run() {
 
     const db = client.db("medicqueue")
     const doctorsCollection = db.collection("doctors")
+    const appointmentCollection = db.collection("appointment")
+    const usersCollection = db.collection("users")
 
     // all doctors data 
     app.get("/doctors", async (req, res) => {
@@ -41,6 +71,52 @@ async function run() {
       res.json(result);
     });
 
+
+
+
+    // all appointment data  read
+    app.get("/appointments", async (req, res) => {
+      const result = await appointmentCollection.find().toArray();
+      res.json(result);
+    });
+
+
+
+    // appointment booking api
+    app.post("/appointment", verifyToken, async (req, res) => {
+      const appointmentData = req.body;
+      const result = await appointmentCollection.insertOne(appointmentData);
+
+      res.status(201).json(result);
+    });
+
+    // appointment delete api
+    app.delete("/appointment/:bookingId", verifyToken, async (req, res) => {
+      const { bookingId } = req.params;
+      const result = await appointmentCollection.deleteOne({
+        _id: new ObjectId(bookingId),
+      });
+
+      res.json(result);
+    });
+
+    // user profile update api
+    app.patch("/users/:email", verifyToken, async (req, res) => {
+      const { email } = req.params;
+      const updatedData = req.body;
+      console.log("Updating user profile:", email, updatedData);
+
+      const result = await usersCollection.updateOne(
+        { email: email },
+        { $set: { ...updatedData, email } },
+        { upsert: true }
+      );
+
+
+      
+
+      res.json(result);
+    });
 
 
 
